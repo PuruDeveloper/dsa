74c4345d8c89ac9dc787c2953834f5f92663129e
purushottam mishra <mpurushottam27@gmail.com>
2024-02-22 23:14:43 +0530

    [1] - Recursion code added.

---
+++ recursion/recursion1.java
@@ -0,0 +1,19 @@
+
+//Print n times with recursion
+import java.util.*;
+
+public class recursion1 {
+    public static void func(int n) {
+        if (n == 0) {
+            return;
+        }
+        System.out.println(n);
+        func(--n);
+    }
+
+    public static void main(String ars[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        func(n);
+    }
+}
